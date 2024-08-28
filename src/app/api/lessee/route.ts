fa86ad1cb273451fed1ed74968dbc0f77446c091
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-08-28 18:22:43 -0300

    perf: remove not used files

--- src/app/api/lessee/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  const data = await request.json();
-
-  return NextResponse.json({
-    data,
-  });
-}
