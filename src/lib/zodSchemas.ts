41593123deac9a2497a55ab1b431285b899adf46
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-08-30 17:02:47 -0300

    feat: create Zod login schema

--- src/lib/zodSchemas.ts
+++ src/lib/zodSchemas.ts
@@ -10,13 +10,21 @@ export const RegisterSchema = z
     password: z
       .string()
       .min(8, { message: "A senha deve possuir no mínimo 8 dígitos" }),
-    confirmPassword: z
-      .string(),
+    confirmPassword: z.string(),
     type: z.string().min(1, { message: "O tipo é obrigatório" }),
-    terms: z.literal(true, { errorMap: () => ({ message: "Os termos devem ser aceitos" }) }),
-
+    terms: z.literal(true, {
+      errorMap: () => ({ message: "Os termos devem ser aceitos" }),
+    }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "As senhas devem ser iguais",
     path: ["confirmPassword"],
   });
+
+export const LoginSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "O endereço de email é obrigatório" })
+    .email({ message: "Email ou senha inválidos" }),
+  password: z.string().min(8, { message: "Email ou senha inválidos" }),
+});
