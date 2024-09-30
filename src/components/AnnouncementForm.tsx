d74c003df435186389fc1018213b6804b9845453
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-09-30 18:56:21 -0300

    chore: import steps

--- src/components/AnnouncementForm.tsx
+++ src/components/AnnouncementForm.tsx
@@ -1,9 +1,65 @@
+"use client";
+
 import IconArrowBack from "@material-design-icons/svg/outlined/arrow_back.svg";
 import AnnouncementSteps from "./AnnouncementSteps";
+import AddressStep from "./AddressStep";
+import AccommodationStep from "./AccommodationStep";
+import AvailableServicesStep from "./AvailableServicesStep";
+import ImagesStep from "./ImagesStep";
+import PropertyStep from "./PropertyStep";
+import PrecificationStep from "./PrecificationStep";
+import { showToast } from "@/lib/utils";
 
 export default function AnnouncementForm() {
+  async function createAnnouncement(e: React.FormEvent<HTMLFormElement>) {
+    try {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+
+      // const data = {
+      //
+      // };
+
+      // const response = await fetch("/api/", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify(data),
+      // });
+
+      // const { errors, message } = await response.json();
+
+      // if (!response.ok) {
+      //   if (errors && Array.isArray(errors)) {
+      //     showToast(
+      //       "error",
+      //       <>
+      //         <p className="mb-2">Por favor, corrija os seguintes erros:</p>
+      //         {errors.map((error: string, index: number) => (
+      //           <p key={index} className="mb-1">
+      //             - {error}
+      //           </p>
+      //         ))}
+      //       </>
+      //     );
+      //   } else if (message) {
+      //     showToast("error", <p>{message}</p>);
+      //   } else {
+      //     showToast("error", <p>Ocorreu um erro desconhecido!</p>);
+      //   }
+      //   return;
+      // }
+
+      // showToast("success", <p>{message}</p>);
+    } catch (error: any) {
+      showToast(
+        "error",
+        <p>{error.message || "Ocorreu um erro ao criar um anúncio!"}</p>
+      );
+    }
+  }
+
   return (
-    <section className="flex flex-col gap-8 w-[calc(100%-4rem)] max-w-screen-md mx-auto ]">
+    <section className="flex flex-col gap-8 w-[calc(100%-4rem)] max-w-screen-md mx-auto relative">
       <button className="flex items-center justify-center bg-light-primary rounded-md w-11 h-11">
         <IconArrowBack
           className="fill-white max-w-5 max-h-5"
@@ -12,25 +68,40 @@ export default function AnnouncementForm() {
         />
       </button>
 
-      <div className="flex gap-8 items-center">
-        <button
-          type="button"
-          aria-label="Voltar"
-          className="py-4 bg-white rounded-lg font-inter font-bold text-[1.125rem]/[1.5rem] border-[0.0625rem] border-light-primary text-light-primary -tracking-[0.0112rem] w-full max-w-36"
-        >
-          Voltar
-        </button>
+      <p className="font-poppins font-normal text-light-on-primary-container text-[1.5rem]/[2rem]">
+        Endereço
+      </p>
 
-        <button
-          type="button"
-          aria-label="Avançar"
-          className="py-4 bg-light-primary rounded-lg font-inter font-bold text-[1.125rem]/[1.5rem] text-white -tracking-[0.0112rem] w-full max-w-36"
-        >
-          Avançar
-        </button>
+      <form onSubmit={createAnnouncement} className="flex flex-col gap-6">
+        <AddressStep />
+        {/* <AccommodationStep /> */}
+        {/* <AvailableServicesStep /> */}
+        {/* <ImagesStep /> */}
+        {/* <PropertyStep /> */}
+        {/* <PrecificationStep /> */}
+      </form>
+
+      <div className="flex flex-col items-center fixed bottom-0 left-0 right-0 w-full gap-8 py-4 bg-white">
+        <div className="flex gap-8 items-center w-full justify-center">
+          <button
+            type="button"
+            aria-label="Voltar"
+            className="py-4 bg-white rounded-lg font-inter font-bold text-[1.125rem]/[1.5rem] border-[0.0625rem] border-light-primary text-light-primary -tracking-[0.0112rem] w-full max-w-36"
+          >
+            Voltar
+          </button>
+
+          <button
+            type="button"
+            aria-label="Avançar"
+            className="py-4 bg-light-primary rounded-lg font-inter font-bold text-[1.125rem]/[1.5rem] text-white -tracking-[0.0112rem] w-full max-w-36"
+          >
+            Avançar
+          </button>
+        </div>
+
+        <AnnouncementSteps />
       </div>
-
-      <AnnouncementSteps />
     </section>
   );
 }
