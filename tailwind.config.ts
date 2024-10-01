b24bea2a2cae17e1ef2010d32089f36990f1af5c
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-10-01 17:58:40 -0300

    chore: change tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,26 +2,25 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/providers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        inter: ['var(--font-inter)'],
-        poppins: ['var(--font-poppins)'],
+        inter: ["var(--font-inter)"],
+        poppins: ["var(--font-poppins)"],
       },
       colors: {
-        'light': {
-          primary: '#006A66',
-          'on-primary-container': '#00201F'
+        light: {
+          primary: "#006A66",
+          "on-primary-container": "#00201F",
         },
-        'dark': {
-          primary: '#80D5D0',
+        dark: {
+          primary: "#80D5D0",
         },
-      }
+      },
     },
   },
   plugins: [],
