2ad2c5dc2a6beefa495a0f54c63a111fcd19db5b
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-10-01 18:00:25 -0300

    chore: new steps types and interfaces

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,3 +1,14 @@
-export interface ToastProviderProps {
+export interface ProviderProps {
   children: React.ReactNode;
+}
+
+export interface Step {
+  key: string;
+  name: string;
+  fields: any[];
+  component: JSX.Element;
+}
+
+export interface StepsContextProviderProps extends ProviderProps {
+  steps: Step[];
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,16 @@
+import { Dispatch, SetStateAction } from "react";
+import { Step } from "./interfaces";
+
 export type ToastType = "success" | "error" | "info" | "warning" | "default";
+
+export type StepsContextType = {
+  previousStep: number;
+  setPreviousStep: Dispatch<SetStateAction<number>>;
+  currentStep: number;
+  setCurrentStep: Dispatch<SetStateAction<number>>;
+  next: () => Promise<void>;
+  prev: () => void;
+  steps: Step[];
+  formData: Record<string, string>;
+  setFormData: Dispatch<SetStateAction<Record<string, string>>>;
+};
