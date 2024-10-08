ce442e3f8c901f1d3570daa301dd952da477b74b
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-10-08 02:17:19 -0300

    chore: add announcement steps

--- src/components/PrecificationStep.tsx
+++ src/components/PrecificationStep.tsx
@@ -1,7 +1,55 @@
+"use client";
+
+import useStepsContext from "@/hooks/useStepsContext";
+import IconEdit from "@material-design-icons/svg/filled/edit.svg";
+
 export default function PrecificationStep() {
+  const { formData, setFormData } = useStepsContext();
+
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    const formatedValue = formatToCurrency(
+      parseFloat(value.replace(/[^\d,]/g, "").replace(",", ".")) || 0
+    );
+
+    setFormData((prev) => ({ ...prev, [name]: formatedValue }));
+  };
+
+  const formatToCurrency = (priceValue: any) => {
+    return priceValue.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    });
+  };
+
   return (
-    <>
-      Precification
-    </>
-  )
+    <div className="flex flex-col gap-4">
+      <label
+        htmlFor="propertyPrice"
+        className="flex flex-col gap-2 font-poppins justify-center items-center font-normal text-2xl text-[#49607B]"
+      >
+        <div className="relative flex items-center gap-2">
+          <input
+            className="min-w-[8.3125rem] max-w-52 outline-none block border-none h-fit text-[2rem] font-bold text-center text-light-primary placeholder-light-primary"
+            type="text"
+            name="propertyPrice"
+            id="propertyPrice"
+            placeholder="R$ 0,00"
+            aria-label="Campo de preço da chácara"
+            value={formData.propertyPrice}
+            onChange={handlePriceChange}
+          />
+
+          <IconEdit
+            className="max-w-6 max-h-6 fill-light-primary cursor-pointer"
+            width={24}
+            height={24}
+          />
+        </div>
+        / dia
+      </label>
+    </div>
+  );
 }
--- src/components/PropertyStep.tsx
+++ src/components/PropertyStep.tsx
@@ -1,7 +1,68 @@
+"use client";
+
+import useStepsContext from "@/hooks/useStepsContext";
+
 export default function PropertyStep() {
+  const { formData, setFormData } = useStepsContext();
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <>
-      Property
-    </>
-  )
+    <div className="flex flex-col gap-4">
+      <label
+        htmlFor="propertyName"
+        className="flex flex-col gap-2 font-poppins font-bold text-base text-[#3B4848] -tracking-[0.019rem]"
+      >
+        Nome da chácara
+        <input
+          className="flex-grow border-[0.0625rem] border-[#6F7978] h-10 p-3 rounded-lg font-normal text-sm placeholder-[#6F7978]"
+          type="text"
+          name="propertyName"
+          id="propertyName"
+          placeholder="Nome da chácara"
+          aria-label="Campo de nome da chácara"
+          value={formData.propertyName}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label
+        htmlFor="announcementTitle"
+        className="flex flex-col gap-2 font-poppins font-bold text-base text-[#3B4848] -tracking-[0.019rem]"
+      >
+        Título do anúncio
+        <input
+          className="flex-grow border-[0.0625rem] border-[#6F7978] h-10 p-3 rounded-lg font-normal text-sm placeholder-[#6F7978]"
+          type="text"
+          name="announcementTitle"
+          id="announcementTitle"
+          placeholder="Título do anúncio"
+          aria-label="Campo de título do anúncio"
+          value={formData.announcementTitle}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label
+        htmlFor="propertyDescription"
+        className="flex flex-col gap-2 font-poppins font-bold text-base text-[#3B4848] -tracking-[0.019rem]"
+      >
+        Descrição
+        <textarea
+          className="resize-none flex-grow border-[0.0625rem] border-[#6F7978] h-48 p-3 rounded-lg font-normal text-sm placeholder-[#6F7978]"
+          name="propertyDescription"
+          id="propertyDescription"
+          placeholder="Descrição do anúncio"
+          aria-label="Campo de descrição"
+          value={formData.propertyDescription}
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
 }
