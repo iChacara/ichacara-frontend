76cc454ca1fed86b41192c361a26fe8e9d16cc39
Gabriel Ragonha <gabrielragonhaprofissional@outlook.com>
2024-10-15 21:10:47 -0300

    feat: create announcement schema

--- src/lib/zodSchemas.ts
+++ src/lib/zodSchemas.ts
@@ -28,3 +28,36 @@ export const LoginSchema = z.object({
     .email({ message: "Email ou senha inválidos" }),
   password: z.string().min(8, { message: "Email ou senha inválidos" }),
 });
+
+export const AnnouncementSchema = z.object({
+  cep: z
+    .string()
+    .min(1, { message: "O CEP é obrigatório" })
+    .length(9, { message: "O CEP deve conter 8 dígitos" }),
+
+  street: z
+    .string()
+    .min(1, { message: "A rua é obrigatória" }),
+
+  number: z
+    .string()
+    .min(1, { message: "O número é obrigatório" })
+    .regex(/^\d+$/, { message: "O número deve ser um valor numérico" }),
+
+  complement: z
+    .string()
+    .optional(),
+
+  neighborhood: z
+    .string()
+    .min(1, { message: "O bairro é obrigatório" }),
+
+  city: z
+    .string()
+    .min(1, { message: "A cidade é obrigatória" }),
+
+  state: z
+    .string()
+    .length(2, { message: "O estado deve conter 2 letras" })
+    .regex(/^[A-Z]+$/, { message: "O estado deve ser composto apenas por letras maiúsculas" }),
+});
