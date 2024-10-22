879f69dbf33e0b85a8977811844512c9a15360ab
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-10-22 19:50:56 -0300

    feat: add farm api routes

---
+++ src/app/api/farm/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { apiClient } from "@/lib/clients";
+import { announcementSchema } from "@/lib/zodSchemas";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const parsedData = announcementSchema.safeParse(body);
+    if (!parsedData.success) {
+      return NextResponse.json(
+        {
+          errors: parsedData.error.issues.map((issue) => issue.message),
+        },
+        { status: 400 }
+      );
+    }
+
+    await apiClient.post(`/farm`, parsedData.data);
+
+    return NextResponse.json({ message: "Chácara criada com sucesso!" });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message:
+          error.response?.data?.message || error.message || "Ocorreu um erro!",
+      },
+      { status: error.response?.data?.statusCode || 500 }
+    );
+  }
+}
