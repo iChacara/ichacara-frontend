9f03cf89f9c73d3d66160c427b0577c29487a29c
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-10-24 19:45:27 -0300

    feat: add ViaCepAddressProps

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,11 @@ import { announcementSchema } from "./zodSchemas";
 export type ToastType = "success" | "error" | "info" | "warning" | "default";
 
 export type AnnouncementFormValues = z.infer<typeof announcementSchema>;
+
+export type ViaCepAddressProps = {
+  bairro: string;
+  complemento: string;
+  uf: string;
+  logradouro: string;
+  localidade: string;
+};
