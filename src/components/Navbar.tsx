2970806c9f710bc60c37aa3a3d62e3b31f2896a0
Joaby Oliveira <joabyoliveira2004@gmail.com>
2024-11-13 01:40:00 -0300

    feat: add notification to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import IconHome from "@material-design-icons/svg/outlined/home.svg";
 import IconCalendarMonth from "@material-design-icons/svg/outlined/calendar_month.svg";
 import IconMenu from "@material-design-icons/svg/outlined/person.svg";
+import IconNotifications from "@material-design-icons/svg/outlined/notifications.svg";
+
 import Link from "next/link";
 
 export default function Navbar() {
@@ -25,6 +27,15 @@ export default function Navbar() {
         >
           <IconCalendarMonth className="fill-inherit" width={28} height={28} />
           <span className="text-sm text-inherit">Agendamentos</span>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"/notificacoes"}
+          className="gap-1 flex flex-col justify-end items-center fill-gray-700 text-gray-700 hover:fill-black hover:text-black transition-colors"
+        >
+          <IconNotifications className="fill-inherit" width={28} height={28} />
+          <span className="text-sm text-inherit">Notificações</span>
         </Link>
       </li>
       <li>
