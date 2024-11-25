569ba07dc474f549951365fb09ef54595a98523e
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-11-25 19:55:25 -0300

    chore: change farms layout

--- src/app/chacara/page.tsx
+++ src/app/chacara/page.tsx
@@ -1,39 +1,73 @@
 import "swiper/css";
 import "swiper/css/navigation";
 import FarmImageCarousel from "../../components/farm/FarmImageCarousel";
-import TagComponent from '../../components/ui/Tag';
+import TagComponent from "../../components/ui/Tag";
 
 export default function ChacaraPage() {
   return (
-    <main className="min-h-dvh h-full p-8">
+    <main className="min-h-dvh h-full overflow-hidden">
       <FarmImageCarousel
         images={[
-          "https://placehold.co/500x500.png",
-          "https://placehold.co/600x500.png",
-          "https://placehold.co/700x500.png",
-          "https://placehold.co/800x500.png",
+          "https://images.unsplash.com/photo-1440558953273-969c107f78a4?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1562866470-3774249bef10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1574714912032-6b7d6d4e9e79?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1689805000702-5d702a1d211b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://plus.unsplash.com/premium_photo-1680302170723-1318f0a8859b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1534177616072-ef7dc120449d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         ]}
       />
-      <div className="flex flex-col gap-2 mt-4">
-        <h1 className="text-2xl font-bold text-gray-800">Chácara bom pingo</h1>
-        <h2 className="text-lg text-gray-600">Marília</h2>
+
+      <div className="max-w-screen-2xl mx-auto w-[calc(100%-4rem)]">
+        <div className="flex flex-col gap-2 mt-4">
+          <h1 className="font-poppins text-[1rem]/[1.25rem] font-medium text-light-primary min-h-10 line-clamp-2 tracking-[0.0156rem]">
+            Chácara Monte dos Guarapanã Chácara Monte dos Guarapanã Chácara
+            Monte dos Guarapanã
+          </h1>
+
+          <h2 className="font-poppins text-[1rem]/[0.0156rem] text-[#6F7978] min-h-5 line-clamp-1 leading-normal">
+            Marília
+          </h2>
+        </div>
+
+        <div className="mt-4 border-b-2 border-b-gray-200 pb-4 flex gap-2">
+          <TagComponent
+            services={[
+              "4 Quartos",
+              "Mesa de sinuca",
+              "Piscina",
+              "Churrasqueira",
+            ]}
+          />
+        </div>
+
+        <p className="font-poppins font-light mt-4 text-light-on-primary-container line-clamp-[10] leading-[1.375rem]">
+          A Chácara Monte dos Guarapanã é um espaço de lazer e eventos situado
+          em um ambiente natural e acolhedor. Com uma ampla área verde, a
+          chácara oferece diversas opções para quem busca tranquilidade, contato
+          com a natureza ou um local especial para realizar eventos como
+          casamentos, festas de aniversário, confraternizações e encontros
+          familiares. A Chácara Monte dos Guarapanã é um espaço de lazer e
+          eventos situado em um ambiente natural e acolhedor. Com uma ampla área
+          verde, a chácara oferece diversas opções para quem busca
+          tranquilidade, contato com a natureza ou um local especial para
+          realizar eventos como casamentos, festas de aniversário,
+          confraternizações e encontros familiares. A Chácara Monte dos
+          Guarapanã é um espaço de lazer e eventos situado em um ambiente
+          natural e acolhedor. Com uma ampla área verde, a chácara oferece
+          diversas opções para quem busca tranquilidade, contato com a natureza
+          ou um local especial para realizar eventos como casamentos, festas de
+          aniversário, confraternizações e encontros familiares.
+        </p>
       </div>
-      <div className="mt-4 border-b-2 border-b-gray-200 pb-4 flex gap-2">
-        <TagComponent
-          services={["4 Quartos", "2 piscinas", "Jacuzzi", "Mesa de sinuca"]}
-        />
-      </div>
-      <p className="mt-4 text-gray-700">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt.{" "}
-      </p>
-      <div className="bg-teal-50 p-8 rounded-2xl flex justify-between items-center mt-8">
-        <strong className="text-2xl text-teal-900 ">R$600/dia</strong>
-        <button className="bg-teal-900 text-teal-50 text-xl rounded-xl p-4">Agendar</button>
+
+      <div className="bg-[#F4FBF9] py-2 px-3 flex w-fit gap-2 items-center fixed bottom-4 left-0 right-0 m-auto">
+        <strong className="font-inter font-normal text-lg text-light-on-primary-container">
+          R$600/dia
+        </strong>
+
+        <button className="flex items-center font-inter text-base bg-light-on-primary-container rounded-lg py-2 px-3 font-medium text-white">
+          Agendar
+        </button>
       </div>
     </main>
   );
--- src/components/farm/FarmImageCarousel.tsx
+++ src/components/farm/FarmImageCarousel.tsx
@@ -1,24 +1,92 @@
 "use client";
-import { SwiperSlide, Swiper } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
-import Image from "next/image";
 
-export default function FarmImageCarousel({ images }: { images: string[] }) {
+import { SwiperSlide, Swiper } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import IconArrowBack from "@material-design-icons/svg/outlined/arrow_back.svg";
+import IconFavorite from "@material-design-icons/svg/outlined/favorite.svg";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import "swiper/css";
+import "swiper/css/pagination";
+interface FarmImageCarouselProps {
+  images: string[];
+}
+
+export default function FarmImageCarousel({ images }: FarmImageCarouselProps) {
+  const router = useRouter();
+
+  const updatePaginationCounter = (
+    swiper: any,
+    element: HTMLElement | null
+  ) => {
+    if (element) {
+      element.textContent = `${swiper.activeIndex + 1}/${images.length}`;
+    }
+  };
+
   return (
-    <Swiper
-      className="h-80 max-h-80 w-full"
-      modules={[Navigation]}
-      navigation={true}
-    >
-      {images.map((image, index) => (
-        <SwiperSlide key={index}>
-          <div>
-            <Image src={image} alt="Faz o L" fill objectFit="cover" />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative w-full h-80">
+      <Swiper
+        className="h-full w-full"
+        modules={[Pagination]}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+          dynamicMainBullets: 5,
+          renderBullet: (index, className) =>
+            `<span class="${className}" style="background-color: #ffffff; transition: all 0.2s ease-in-out"></span>`,
+        }}
+        onSwiper={(swiper) => {
+          const paginationCounter = document.querySelector(
+            ".swiper-pagination-count"
+          ) as HTMLElement | null;
+          updatePaginationCounter(swiper, paginationCounter);
+        }}
+        onSlideChange={(swiper) => {
+          const paginationCounter = document.querySelector(
+            ".swiper-pagination-count"
+          ) as HTMLElement | null;
+          updatePaginationCounter(swiper, paginationCounter);
+        }}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index} aria-label={`Slide ${index + 1}`}>
+            <div className="relative h-full w-full">
+              <Image
+                src={image}
+                alt={`Slide ${index + 1}`}
+                fill
+                objectFit="cover"
+                priority
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <p className="swiper-pagination-count min-w-[3.375rem] font-inter text-sm font-medium absolute bottom-7 left-0 right-0 m-auto z-10 text-light-primary bg-[#F4FBF9] flex items-center justify-center w-fit py-2 px-3 rounded-lg">
+        1/{images.length}
+      </p>
+
+      <div className="absolute z-10 top-2 left-0 right-0 w-full max-w-[calc(100%-1rem)] m-auto flex justify-between">
+        <button
+          onClick={() => router.back()}
+          className="bg-light-on-primary-container p-2 rounded-full shadow-lg hover:bg-light-primary transition duration-200"
+        >
+          <IconArrowBack className="fill-white" width={16} height={16} />
+        </button>
+
+        <button
+          onClick={() => console.log("Favoritos")}
+          className="bg-light-on-primary-container p-2 rounded-full shadow-lg hover:bg-light-primary transition duration-200"
+        >
+          <IconFavorite
+            className="stroke-white fill-transparent"
+            width={16}
+            height={16}
+          />
+        </button>
+      </div>
+    </div>
   );
 }
