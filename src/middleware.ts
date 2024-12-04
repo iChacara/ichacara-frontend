14d3561575a4d8e40a87168e281fe53ad3381040
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2024-12-04 02:47:11 -0300

    chore: add routes validation by user type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import getCookie from "./hooks/useCookie";
+import { getUser } from "./services/user";
 
 export const config = {
   matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const token = getCookie("ichacara_token");
   const pathname = request.nextUrl.pathname;
 
+  const publicRoutes = ["/", "/criar-conta"];
+
   if (token) {
-    if (pathname === "/" || pathname === "/criar-conta") {
-      return NextResponse.redirect(new URL("/chacaras", request.url));
+    const user = await getUser();
+
+    if (user) {
+      if (publicRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL("/chacaras", request.url));
+      }
+
+      const protectedRoutes = {
+        lessor: ["/anunciar-chacara"],
+      };
+
+      if (protectedRoutes.lessor.includes(pathname) && user.type !== "lessor") {
+        return NextResponse.redirect(new URL("/chacaras", request.url));
+      }
+
+      return NextResponse.next();
     }
   }
 
-  if (!token) {
-    if (pathname !== "/" && pathname !== "/criar-conta") {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+  if (!token && !publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
